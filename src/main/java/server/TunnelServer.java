27e9443b1c88f03ad833d4c624770473936721f5
Manoj Inukolunu <minukolunu@minukol-ltmvxv6.internal.salesforce.com>
2022-09-17 12:16:56 +0530

    Add Cronitor Integration

--- src/main/java/server/TunnelServer.java
+++ src/main/java/server/TunnelServer.java
@@ -2,6 +2,7 @@ package server;
 
 import codec.ProtoMessageDecoder;
 import codec.ProtoMessageEncoder;
+import io.cronitor.client.CronitorClient;
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelInitializer;
@@ -17,7 +18,14 @@ import io.netty.handler.codec.http.HttpObjectAggregator;
 import io.netty.handler.codec.http.HttpRequestEncoder;
 import io.netty.handler.logging.LogLevel;
 import io.netty.handler.logging.LoggingHandler;
+
+import java.io.IOException;
 import java.util.UUID;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import lombok.extern.log4j.Log4j2;
 import server.handler.AckMessageHandler;
 import server.handler.MessageHandlers;
 import server.handler.RegisterMessageHandler;
@@ -25,48 +33,67 @@ import server.handler.ResponseMessageHandler;
 import server.handler.TunnelServerHandler;
 import server.http.HttpServer;
 
+
 public class TunnelServer {
 
-  public static void main(String[] args) {
-    EventLoopGroup bossGroup = new NioEventLoopGroup();
-    EventLoopGroup workerGroup = new NioEventLoopGroup();
-    try {
-      ServerBootstrap bootstrap = new ServerBootstrap();
-      bootstrap.group(bossGroup, workerGroup)
-          .channel(NioServerSocketChannel.class)
-          .option(ChannelOption.SO_BACKLOG, 1024)
-          .handler(new LoggingHandler(LogLevel.INFO))
-          .childHandler(new ChannelInitializer<SocketChannel>() {
-            @Override
-            protected void initChannel(SocketChannel ch) throws Exception {
-              ChannelPipeline pipeline = ch.pipeline();
-              MessageHandlers handlers = new MessageHandlers();
-              handlers.register(MessageTypeEnum.REGISTER, new RegisterMessageHandler());
-              handlers.register(MessageTypeEnum.HTTP_RESPONSE, new ResponseMessageHandler());
-              handlers.register(MessageTypeEnum.ACK, new AckMessageHandler());
-              addProtoClientHandlers(pipeline, handlers);
-            }
-          });
 
-      ChannelFuture future = bootstrap.bind(8585).sync();
-      HttpServer server = new HttpServer();
-      server.run();
+    public static void main(String[] args) {
+
+        ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+        scheduler.schedule(TunnelServer::cronitorHeartBeat, 1, TimeUnit.MINUTES);
+
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap bootstrap = new ServerBootstrap();
+            bootstrap.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .option(ChannelOption.SO_BACKLOG, 1024)
+                    .handler(new LoggingHandler(LogLevel.INFO))
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            ChannelPipeline pipeline = ch.pipeline();
+                            MessageHandlers handlers = new MessageHandlers();
+                            handlers.register(MessageTypeEnum.REGISTER, new RegisterMessageHandler());
+                            handlers.register(MessageTypeEnum.HTTP_RESPONSE, new ResponseMessageHandler());
+                            handlers.register(MessageTypeEnum.ACK, new AckMessageHandler());
+                            addProtoClientHandlers(pipeline, handlers);
+                        }
+                    });
+
+            ChannelFuture future = bootstrap.bind(8585).sync();
+            HttpServer server = new HttpServer();
+            server.run();
 //      future.channel().closeFuture().sync();
-    } catch (InterruptedException e) {
-      e.printStackTrace();
-    } finally {
-      bossGroup.shutdownGracefully();
-      workerGroup.shutdownGracefully();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
     }
-  }
 
-  public static void addProtoClientHandlers(ChannelPipeline pipeline, MessageHandlers handlers) {
-    pipeline.addLast("length-decoder", new LengthFieldBasedFrameDecoder(Integer.MAX_VALUE, 0, 4, 0, 4));
-    pipeline.addLast("length-prepender", new LengthFieldPrepender(4));
-    pipeline.addLast("proto-message-encoder", new ProtoMessageEncoder());
-    pipeline.addLast("proto-message-decoder", new ProtoMessageDecoder());
-    pipeline.addLast("tunnel-server-handler", new TunnelServerHandler(handlers));
-  }
+    private static void cronitorHeartBeat() {
+        try {
+            CronitorClient cronitorClient = new CronitorClient("d2c7f7b9bd0b4c3b95aa666ce629aba3");
+            try {
+                cronitorClient.tick("JTunnel", "Jtunnel server alive");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void addProtoClientHandlers(ChannelPipeline pipeline, MessageHandlers handlers) {
+        pipeline.addLast("length-decoder", new LengthFieldBasedFrameDecoder(Integer.MAX_VALUE, 0, 4, 0, 4));
+        pipeline.addLast("length-prepender", new LengthFieldPrepender(4));
+        pipeline.addLast("proto-message-encoder", new ProtoMessageEncoder());
+        pipeline.addLast("proto-message-decoder", new ProtoMessageDecoder());
+        pipeline.addLast("tunnel-server-handler", new TunnelServerHandler(handlers));
+    }
 
 }
 
--- src/main/java/server/http/HttpServer.java
+++ src/main/java/server/http/HttpServer.java
@@ -15,26 +15,30 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class HttpServer implements Runnable {
 
-  public void run() {
-    log.info("Starting HTTP Server");
-    EventLoopGroup bossGroup = new NioEventLoopGroup(1);
-    EventLoopGroup workerGroup = new NioEventLoopGroup();
-    try {
-      ServerBootstrap b = new ServerBootstrap();
-      b.option(ChannelOption.SO_BACKLOG, 1024);
-      b.group(bossGroup, workerGroup)
-          .channel(NioServerSocketChannel.class)
-          .handler(new LoggingHandler(LogLevel.INFO))
-          .childHandler(new HttpServerChannelInitializer());
-      Channel ch = b.bind(8080).sync().channel();
-      ch.closeFuture().sync();
-    } catch (InterruptedException e) {
-      e.printStackTrace();
-    } finally {
-      bossGroup.shutdownGracefully();
-      workerGroup.shutdownGracefully();
+
+
+
+
+    public void run() {
+        log.info("Starting HTTP Server");
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.option(ChannelOption.SO_BACKLOG, 1024);
+            b.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .handler(new LoggingHandler(LogLevel.INFO))
+                    .childHandler(new HttpServerChannelInitializer());
+            Channel ch = b.bind(8080).sync().channel();
+            ch.closeFuture().sync();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
     }
-  }
 }
 
 
